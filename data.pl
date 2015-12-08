d9bcc4c9ef8736f94e66f92b77df1ce22482431b
dreymaior <andrey.maior@gmail.com>
2015-12-07 23:00:06 -0200

    DONE

---
+++ data.pl
@@ -0,0 +1,105 @@
+vertex(maringa).
+vertex(portoRico).
+vertex(paranavai).
+vertex(cidadeGaucha).
+vertex(cruzeiroDoOeste).
+vertex(umuarama).
+vertex(assisChateubriand).
+vertex(guaira).
+vertex(campoMourao).
+vertex(cascavel).
+vertex(fozDoIguacu).
+vertex(franciscoBeltrao).
+vertex(patoBranco).
+vertex(guarapuava).
+vertex(uniaoDaVitoria).
+vertex(irati).
+vertex(pontaGrossa).
+vertex(manoelRibas).
+vertex(curitiba).
+vertex(paranagua).
+vertex(cerroAzul).
+vertex(jaguariaiva).
+vertex(ibaiti).
+vertex(jacarezinho).
+vertex(cornelioProcopio).
+vertex(londrina).
+vertex(sertanopolis).
+vertex(santoInacio).
+vertex(apucarana).
+vertex(mauaDaSerra).
+vertex(imbau).
+vertex(marialva).
+vertex(lunardeli).
+vertex(boaVistaDaAparecida).
+vertex(chopinzinho).
+edge(r1, maringa, marialva, 17, settings(5,0,110)).
+edge(r2,maringa, santoInacio, 91, settings(3,0,80)).
+edge(r3,maringa, paranavai, 74, settings(3,6.8,110)).
+edge(r4,maringa, cidadeGaucha, 146, settings(2,0,60)).
+edge(r5,maringa, campoMourao, 91, settings(5,6.8,110)).
+edge(r6,portoRico, santoInacio, 208, settings(2,0,60)).
+edge(r7, portoRico, paranavai, 102, settings(3,0,80)).
+edge(r8, portoRico, cidadeGaucha, 95, settings(3,0,60)).
+edge(r9, portoRico, umuarama, 140, settings(4,0,90)).
+edge(r10, paranavai, santoInacio, 115, settings(2,0,60)).
+edge(r11, paranavai, cidadeGaucha, 79, settings(3,0,80)).
+edge(r12, cidadeGaucha, umuarama, 70, settings(4,0,110)).
+edge(r13, cidadeGaucha, cruzeiroDoOeste, 76, settings(3,0,110)).
+edge(r14, cruzeiroDoOeste, umuarama, 29, settings(5,0,110)).
+edge(r15, cruzeiroDoOeste, assisChateubriand, 112, settings(3,0,80)).
+edge(r16, cruzeiroDoOeste, campoMourao, 88, settings(4,0,110)).
+edge(r17, umuarama, guaira, 115, settings(4,0,90)).
+edge(r18, assisChateubriand, guaira, 114, settings(3,0,70)).
+edge(r19, assisChateubriand, campoMourao, 144, settings(3,0,80)).
+edge(r20, assisChateubriand, cascavel, 179, settings(3,0,60)).
+edge(r21, guaira, fozDoIguacu, 223, settings(2,0,60)).
+edge(r22, campoMourao, manoelRibas, 117, settings(2,0,60)).
+edge(r23, campoMourao, lunardeli, 100, settings(3,0,80)).
+edge(r24, cascavel, fozDoIguacu, 139, settings(5,13.6,110)).
+edge(r25, cascavel, guarapuava, 248, settings(5,20.4,110)).
+edge(r26, cascavel, boaVistaDaAparecida, 73, settings(1,0,50)).
+edge(r27, fozDoIguacu, fransciscoBeltrao, 283, settings(3,0,80)).
+edge(r28, fozDoIguacu, boaVistaDaAparecida, 206, settings(2,0,60)).
+edge(r29, franciscoBeltrao, patoBranco, 55, settings(4,0,80)).
+edge(r30, franciscoBeltrao, boaVistaDaAparecida, 123, settings(2,0,60)).
+edge(r31, franciscoBeltrao, chopinzinho, 88, settings(1,0,50)).
+edge(r32, patoBranco, uniaoDaVitoria, 218, settings(3,0,80)).
+edge(r33, patoBranco, chopinzinho, 59, settings(2,0,60)).
+edge(r34, guarapuava, uniaoDaVitoria, 203, settings(4,0,80)).
+edge(r35, guarapuava, irati, 104, settings(5,13.6,110)).
+edge(r36, guarapuava, manoelRibas, 122, settings(3,0,80)).
+edge(r37, guarapuava, chopinzinho, 152, settings(4,6.8,80)).
+edge(r38, uniaoDaVitoria, irati, 122, settings(3,0,60)).
+edge(r39, uniaoDaVitoria, curitiba, 235, settings(2,0,60)).
+edge(r40, irati, pontaGrossa, 84, settings(4,0,90)).
+edge(r41, irati, curitba, 156, settings(5,13.6,110)).
+edge(r42, pontaGrossa, manoelRibas, 223, settings(3,6.8,80)).
+edge(r43, pontaGrossa, curitiba, 114, settings(5,13.6,110)).
+edge(r44, pontaGrossa, jaguariaiva, 125, settings(4,13.6,110)).
+edge(r45, pontaGrossa, imbau, 112, settings(5,6.8,110)).
+edge(r46, manoelRibas, imbau, 138, settings(3,0,80)).
+edge(r47, manoelRibas, lunardeli, 66, seetings(1,0,50)).
+edge(r48, curitiba, paranagua, 90, settings(5,27.3,110)).
+edge(r49, curitiba, cerroAzul, 86, settings(2,0,60)).
+edge(r50, cerroAzul, jaguariaiva, 104, settings(3,0,80)).
+edge(r51, jaguariaiva, ibaiti, 115, settings(4,0,90)).
+edge(r52, jaguariaiva, imbau, 168, settings(3,0,80)).
+edge(r53, ibaiti, jacarezinho, 88, settings(4,0,90)).
+edge(r54, ibaiti, cornelioProcopio, 108, settings(3,0,80)).
+edge(r55, ibaiti, imbau, 126, settings(4,0,80)).
+edge(r56, jacarezinho, cornelioProcopio, 89, settings(5,6.8,110)).
+edge(r57, cornelioProcopio, londrina, 67, settings(5,6.8,110)).
+edge(r58, cornelioProcopio, sertanopolis, 69, settings(4,0,90)).
+edge(r59, londrina, sertanopolis, 44, settings(3,0,80)).
+edge(r60, londrina, santoInacio, 117, settings(3,0,80)).
+edge(r61, londrina, apucarana, 54, settings(5,6.8,110)).
+edge(r62, londrina, mauaDaSerra, 83, settings(3,0,80)).
+edge(r63, londrina, marialva, 80,settings(5,13.6,110)).
+edge(r64, sertanopolis, santoInacio, 113, settings(2,0,60)).
+edge(r65, apucarana, mauaDaSerra, 52, settings(5,0,110)).
+edge(r66, apucarana, marialva, 45, settings(5,6.8,110)).
+edge(r67, mauaDaSerra, imbau, 96, settings(5,13.6,110)).
+edge(r68, mauaDaSerra, lunardeli, 90, settings(3,0,80)).
+edge(r69, marialva, lunardeli, 97, settings(3,6.8,80)).
+edge(r70, boaVistaDaAparecida, chopinzinho, 167, settings(2,0,60)).
